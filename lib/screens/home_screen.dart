03af075673e7a210466992ca0edfd65dbe7b74e2
Prashant <prashant78907890@gmail.com>
2026-05-14 22:26:39 +0530

    added two scrrens and navigation inbetween them

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/screens/nearby_screen.dart';
+import 'package:restaurant_app/screens/search_map_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _screens = [const NearbyScreen(), const SearchMapScreen()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(label: 'home', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+            label: 'settings',
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
+    );
+  }
+}
