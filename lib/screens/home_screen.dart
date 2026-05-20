6612c423746f62f1bfe2176a3d66e4e41ea4e2c8
Prashant <prashant78907890@gmail.com>
2026-05-20 22:55:41 +0530

    Added firebase

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_app/screens/nearby_screen.dart';
 import 'package:restaurant_app/screens/search_map_screen.dart';
+import 'package:restaurant_app/screens/user_profile.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,7 +12,11 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
-  final List<Widget> _screens = [NearbyScreen(), SearchMapScreen()];
+  final List<Widget> _screens = [
+    NearbyScreen(),
+    SearchMapScreen(),
+    UserProfile(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,9 +30,10 @@ class _HomeScreenState extends State<HomeScreen> {
         },
         items: [
           BottomNavigationBarItem(label: 'home', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: 'Search', icon: Icon(Icons.search)),
           BottomNavigationBarItem(
-            label: 'settings',
-            icon: Icon(Icons.settings),
+            label: 'User',
+            icon: Icon(Icons.verified_user_rounded),
           ),
         ],
       ),
